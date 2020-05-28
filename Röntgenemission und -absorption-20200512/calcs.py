cfd9a9b9291f19b7ca6cf1f78fbdfd6fc394f366
Richard Leven <richard.leven@udo.edu>
2020-05-28 18:25:01 +0200

    Corrected version

--- Röntgenemission und -absorption-20200512/calcs.py
+++ Röntgenemission und -absorption-20200512/calcs.py
@@ -2,7 +2,7 @@ import numpy as np
 from uncertainties import ufloat, unumpy
 from scipy.constants import constants
 
-ry = constants.Rydberg
+ry = 13.6
 a = constants.alpha
 h = constants.h
 c = constants.c
@@ -21,9 +21,18 @@ def lam(en):
     return h*c/(en*ev)*10**(12)
 
 def sigmak(z, abst):
-    return z-np.sqrt((abst/ry)+(a*a/4)*(z**4))
+    return z-np.sqrt((abst/ry)-(a*a/4)*(z**4))
+
+def sigma1(z, abst):
+    return z-np.sqrt(abst/ry)
+
+def sigma2(z, ekal, sig):
+    return z-np.sqrt(4*(z-sig)**2-4*ekal/ry)
+
+def sigma3(z, ebet, sig):
+    return z-np.sqrt(9*(z-sig)**2-9*ebet/ry)
 
 def intense(mi, ma):
     return mi+(ma-mi)/2
 
-print(engy(wave(17.35)))
+print(sigmak(30, 9625))
